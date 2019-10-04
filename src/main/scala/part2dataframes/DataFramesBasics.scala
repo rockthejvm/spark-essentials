634d23d2685b18c7fc8a3d69cc17727f7c5efa57
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-10-04 15:24:14 +0300

    dataframes basics exercise

--- src/main/scala/part2dataframes/DataFramesBasics.scala
+++ src/main/scala/part2dataframes/DataFramesBasics.scala
@@ -74,4 +74,34 @@ object DataFramesBasics extends App {
   val manualCarsDFWithImplicits = cars.toDF("Name", "MPG", "Cylinders", "Displacement", "HP", "Weight", "Acceleration", "Year", "CountryOrigin")
 
 
+  /**
+    * Exercise:
+    * 1) Create a manual DF describing smartphones
+    *   - make
+    *   - model
+    *   - screen dimension
+    *   - camera megapixels
+    *
+    * 2) Read another file from the data/ folder, e.g. movies.json
+    *   - print its schema
+    *   - count the number of rows, call count()
+    */
+
+  // 1
+  val smartphones = Seq(
+    ("Samsung", "Galaxy S10", "Android", 12),
+    ("Apple", "iPhone X", "iOS", 13),
+    ("Nokia", "3310", "THE BEST", 0)
+  )
+
+  val smartphonesDF = smartphones.toDF("Make", "Model", "Platform", "CameraMegapixels")
+  smartphonesDF.show()
+
+  // 2
+  val moviesDF = spark.read
+    .format("json")
+    .option("inferSchema", "true")
+    .load("src/main/resources/data/movies.json")
+  moviesDF.printSchema()
+  println(s"The Movies DF has ${moviesDF.count()} rows")
 }
