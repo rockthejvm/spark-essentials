2d4086cf43444b932d10ebe251b3ef790e4cb517
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-10-08 15:17:56 +0300

    datasources part 2

--- src/main/scala/part2dataframes/DataSources.scala
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -18,7 +18,7 @@ object DataSources extends App {
     StructField("Horsepower", LongType),
     StructField("Weight_in_lbs", LongType),
     StructField("Acceleration", DoubleType),
-    StructField("Year", StringType),
+    StructField("Year", DateType),
     StructField("Origin", StringType)
   ))
 
@@ -57,4 +57,79 @@ object DataSources extends App {
     .format("json")
     .mode(SaveMode.Overwrite)
     .save("src/main/resources/data/cars_dupe.json")
+
+  // JSON flags
+  spark.read
+    .schema(carsSchema)
+    .option("dateFormat", "YYYY-MM-dd") // couple with schema; if Spark fails parsing, it will put null
+    .option("allowSingleQuotes", "true")
+    .option("compression", "uncompressed") // bzip2, gzip, lz4, snappy, deflate
+    .json("src/main/resources/data/cars.json")
+
+  // CSV flags
+  val stocksSchema = StructType(Array(
+    StructField("symbol", StringType),
+    StructField("date", DateType),
+    StructField("price", DoubleType)
+  ))
+
+  spark.read
+    .schema(stocksSchema)
+    .option("dateFormat", "MMM dd YYYY")
+    .option("header", "true")
+    .option("sep", ",")
+    .option("nullValue", "")
+    .csv("src/main/resources/data/stocks.csv")
+
+  // Parquet
+  carsDF.write
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars.parquet")
+
+  // Text files
+  spark.read.text("src/main/resources/data/sampleTextFile.txt").show()
+
+  // Reading from a remote DB
+  val driver = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5432/rtjvm"
+  val user = "docker"
+  val password = "docker"
+
+  val employeesDF = spark.read
+    .format("jdbc")
+    .option("driver", driver)
+    .option("url", url)
+    .option("user", user)
+    .option("password", password)
+    .option("dbtable", "public.employees")
+    .load()
+
+  /**
+    * Exercise: read the movies DF, then write it as
+    * - tab-separated values file
+    * - snappy Parquet
+    * - table "public.movies" in the Postgres DB
+    */
+
+  val moviesDF = spark.read.json("src/main/resources/data/movies.json")
+
+  // TSV
+  moviesDF.write
+    .format("csv")
+    .option("header", "true")
+    .option("sep", "\t")
+    .save("src/main/resources/data/movies.csv")
+
+  // Parquet
+  moviesDF.write.save("src/main/resources/data/movies.parquet")
+
+  // save to DF
+  moviesDF.write
+    .format("jdbc")
+    .option("driver", driver)
+    .option("url", url)
+    .option("user", user)
+    .option("password", password)
+    .option("dbtable", "public.movies")
+    .save()
 }
