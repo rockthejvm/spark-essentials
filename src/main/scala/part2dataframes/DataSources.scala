262a50840373784cca58a38508c31861cced495c
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-10-08 13:54:38 +0300

    datasources

---
+++ src/main/scala/part2dataframes/DataSources.scala
@@ -0,0 +1,60 @@
+package part2dataframes
+
+import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.apache.spark.sql.types._
+
+object DataSources extends App {
+
+  val spark = SparkSession.builder()
+    .appName("Data Sources and Formats")
+    .config("spark.master", "local")
+    .getOrCreate()
+
+  val carsSchema = StructType(Array(
+    StructField("Name", StringType),
+    StructField("Miles_per_Gallon", DoubleType),
+    StructField("Cylinders", LongType),
+    StructField("Displacement", DoubleType),
+    StructField("Horsepower", LongType),
+    StructField("Weight_in_lbs", LongType),
+    StructField("Acceleration", DoubleType),
+    StructField("Year", StringType),
+    StructField("Origin", StringType)
+  ))
+
+  /*
+    Reading a DF:
+    - format
+    - schema or inferSchema = true
+    - path
+    - zero or more options
+   */
+  val carsDF = spark.read
+    .format("json")
+    .schema(carsSchema) // enforce a schema
+    .option("mode", "failFast") // dropMalformed, permissive (default)
+    .option("path", "src/main/resources/data/cars.json")
+    .load()
+
+  // alternative reading with options map
+  val carsDFWithOptionMap = spark.read
+    .format("json")
+    .options(Map(
+      "mode" -> "failFast",
+      "path" -> "src/main/resources/data/cars.json",
+      "inferSchema" -> "true"
+    ))
+    .load()
+
+  /*
+   Writing DFs
+   - format
+   - save mode = overwrite, append, ignore, errorIfExists
+   - path
+   - zero or more options
+  */
+  carsDF.write
+    .format("json")
+    .mode(SaveMode.Overwrite)
+    .save("src/main/resources/data/cars_dupe.json")
+}
