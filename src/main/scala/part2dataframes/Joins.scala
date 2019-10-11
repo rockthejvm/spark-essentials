3400de43dc41ea5f90924099fbf9448c174e6f7e
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-10-11 22:30:00 +0300

    joins exercise

--- src/main/scala/part2dataframes/Joins.scala
+++ src/main/scala/part2dataframes/Joins.scala
@@ -59,5 +59,49 @@ object Joins extends App {
   // using complex types
   guitaristsDF.join(guitarsDF.withColumnRenamed("id", "guitarId"), expr("array_contains(guitars, guitarId)"))
 
+  /**
+    * Exercises
+    *
+    * 1. show all employees and their max salary
+    * 2. show all employees who were never managers
+    * 3. find the job titles of the best paid 10 employees in the company
+    */
+
+  val driver = "org.postgresql.Driver"
+  val url = "jdbc:postgresql://localhost:5432/rtjvm"
+  val user = "docker"
+  val password = "docker"
+
+  def readTable(tableName: String) = spark.read
+    .format("jdbc")
+    .option("driver", driver)
+    .option("url", url)
+    .option("user", user)
+    .option("password", password)
+    .option("dbtable", s"public.$tableName")
+    .load()
+
+  val employeesDF = readTable("employees")
+  val salariesDF = readTable("salaries")
+  val deptManagersDF = readTable("dept_manager")
+  val titlesDF = readTable("titles")
+
+  // 1
+  val maxSalariesPerEmpNoDF = salariesDF.groupBy("emp_no").agg(max("salary").as("maxSalary"))
+  val employeesSalariesDF = employeesDF.join(maxSalariesPerEmpNoDF, "emp_no")
+
+  // 2
+  val empNeverManagersDF = employeesDF.join(
+    deptManagersDF,
+    employeesDF.col("emp_no") === deptManagersDF.col("emp_no"),
+    "left_anti"
+  )
+
+  // 3
+  val mostRecentJobTitlesDF = titlesDF.groupBy("emp_no", "title").agg(max("to_date"))
+  val bestPaidEmployeesDF = employeesSalariesDF.orderBy(col("maxSalary").desc).limit(10)
+  val bestPaidJobsDF = bestPaidEmployeesDF.join(mostRecentJobTitlesDF, "emp_no")
+
+  bestPaidJobsDF.show()
 }
 
