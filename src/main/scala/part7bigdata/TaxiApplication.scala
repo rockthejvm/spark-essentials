67b1aa786bfa184245064ee7854d17a26f5e97f7
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-10-23 19:13:59 +0300

    taxi project start

---
+++ src/main/scala/part7bigdata/TaxiApplication.scala
@@ -0,0 +1,24 @@
+package part7bigdata
+
+import org.apache.spark.sql.SparkSession
+
+object TaxiApplication extends App {
+
+  val spark = SparkSession.builder()
+    .config("spark.master", "local")
+    .appName("Taxi Big Data Application")
+    .getOrCreate()
+
+  val bigTaxiDF = spark.read.load("/Users/daniel/Downloads/NYC_taxi_2009-2016.parquet")
+
+  val taxiDF = spark.read.load("src/main/resources/data/yellow_taxi_jan_25_2018")
+  taxiDF.printSchema()
+
+  val taxiZonesDF = spark.read
+    .option("header", "true")
+    .option("inferSchema", "true")
+    .csv("src/main/resources/data/taxi_zones.csv")
+  taxiZonesDF.printSchema()
+
+
+}
